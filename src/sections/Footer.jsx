f49ba7348c590f9d30512f7c2a7cea7f14f383f7
allzxxopemula <allzxxott@gmail.com>
2026-05-04 15:54:16 +0700

    linkbru

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -55,7 +55,7 @@ const Footer = () => {
         <div className="footer-bottom">
           <hr className="footer-divider" />
           <div className="footer-copyright">
-            <p>&copy; {currentYear} <span>Nusantara Eather</span>. All rights reserved | By <a href="allzxxosite.vercel.app" target="_blank" rel="noreferrer">Allzxxo Dev</a></p>
+            <p>&copy; {currentYear} <span>Nusantara Eather</span>. All rights reserved | By <a href="https://allzxxosite.vercel.app/" target="_blank" rel="noreferrer">Allzxxo Dev</a></p>
             <p className="dev-credit">Designed for Nusantara Community</p>
           </div>
         </div>
